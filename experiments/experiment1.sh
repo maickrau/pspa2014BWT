a86534f538affe25a60d8404012536e7e6c007a2
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-02-14 13:22:18 +0200

    sketched out poster and experiments, made scripts for running experiment 1

---
+++ experiments/experiment1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+runInFileTest(){
+	/usr/bin/time -f "%U %S" ./bwt -f -M $2 -i $1 -o tmpout.out
+	rm tmpout.out
+}
+
+runTestsWithMemories(){
+	runInFileTest $1 $(($2*1/4))  # 25% of input size
+	runInFileTest $1 $(($2*2/4))  # 50% of input size
+	runInFileTest $1 $(($2*3/4))  # 75% of input size
+	runInFileTest $1 $(($2*1))    # 100% of input size
+	runInFileTest $1 $(($2*5/4))  # 125% of input size
+	runInFileTest $1 $(($2*6/4))  # 150% of input size
+	runInFileTest $1 $(($2*8/4))  # 200% of input size
+	runInFileTest $1 $(($2*10/4)) # 250% of input size
+	runInFileTest $1 $(($2*12/4)) # 300% of input size
+	runInFileTest $1 $(($2*16/4)) # 400% of input size
+}
+
+runTestsWithMemories genome10 10000000
+runTestsWithMemories genome50 50000000
+runTestsWithMemories genome100 100000000
+runTestsWithMemories genome250 250000000
+runTestsWithMemories genome500 500000000
+
+runTestsWithMemories quote10 10000000
+runTestsWithMemories quote50 50000000
+runTestsWithMemories quote100 100000000
+runTestsWithMemories quote250 250000000
+
+runTestsWithMemories random10 10000000
+runTestsWithMemories random50 50000000
+runTestsWithMemories random100 100000000
+
+runTestsWithMemories repeat10 10000000
+runTestsWithMemories repeat50 50000000
+runTestsWithMemories repeat100 100000000
---
+++ experiments/make_datas.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+echo "downloading human genome"
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit
+echo "downloading twoBitToFa"
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
+echo "converting genome to .fa"
+chmod u+x ./twoBitToFa
+./twoBitToFa hg19.2bit hg19.fa
+echo "making 10 Mb genome"
+tr -cd 'ATCGN' < hg19.fa | head -c 10000000 > genome10
+echo "making 50 Mb genome"
+tr -cd 'ATCGN' < hg19.fa | head -c 50000000 > genome50
+echo "making 100 Mb genome"
+tr -cd 'ATCGN' < hg19.fa | head -c 100000000 > genome100
+echo "making 250 Mb genome"
+tr -cd 'ATCGN' < hg19.fa | head -c 250000000 > genome250
+echo "making 500 Mb genome"
+tr -cd 'ATCGN' < hg19.fa | head -c 500000000 > genome500
+echo "removing big .fa file"
+rm hg19.fa
+echo "removing twoBitToFa"
+rm twoBitToFa
+echo "downloading wikiquote dump"
+wget dumps.wikimedia.org/enwikiquote/20140208/enwikiquote-20140208-pages-articles-multistream.xml.bz2
+echo "unpacking wikiquote dump"
+bunzip2 enwikiquote-20140208-pages-articles-multistream.xml.bz2
+mv enwikiquote-20140208-pages-articles-multistream.xml wikiquote
+echo "making 10 Mb quotes"
+tr -d '\0' < wikiquote | head -c 10000000 > quote10
+echo "making 50 Mb quotes"
+tr -d '\0' < wikiquote | head -c 50000000 > quote50
+echo "making 100 Mb quotes"
+tr -d '\0' < wikiquote | head -c 100000000 > quote100
+echo "making 250 Mb quotes"
+tr -d '\0' < wikiquote | head -c 250000000 > quote250
+echo "removing big wikiquote file"
+rm wikiquote
+echo "creating 10 Mb random file"
+tr -d '\0' < /dev/urandom | head -c 10000000 > random10
+echo "creating 50 Mb random file"
+tr -d '\0' < /dev/urandom | head -c 50000000 > random50
+echo "creating 100 Mb random file"
+tr -d '\0' < /dev/urandom | head -c 100000000 > random100
+echo "creating 10 Mb repeating 'a' file"
+seq -sa 10000001 | tr -d '\n[:digit:]' > repeat10
+echo "creating 50 Mb repeating 'a' file"
+seq -sa 50000001 | tr -d '\n[:digit:]' > repeat50
+echo "creating 100 Mb repeating 'a' file"
+seq -sa 100000001 | tr -d '\n[:digit:]' > repeat100
